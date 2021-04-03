094302bbbf82d5dbe8a08b26553b16a2bf30ed6f
nison79 <georgenison@gmail.com>
2021-04-03 14:22:22 +0300

    Header without Slider

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Menu from './components/Menu'
 import NewsLetter from './components/NewsLetter';
 import Services from './components/Services'
 import Footer from './components/Footer'
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Header />
       <Services />
       <NewsLetter />
       <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,12 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='header-wrapper'>
+      {/* <Slider /> */}
+      <div className="action">
+        <h2>There is no other <br/>platforms for you as <br/>like</h2>
+        <button className="header-button">TRY NOW</button>
+      </div>
     </div>
   )
 }
